1e9f64b7e00fbb604241e1fa3e22d73f29d35db4
joubin <joubin.j@gmail.com>
2013-04-16 00:19:00 -0700

    Finished my inmplementation of link lists
    This is written so that it can be included with any other file and
    fucntion calls can be mad.

--- LinkLists.c
+++ LinkLists.c
@@ -2,57 +2,25 @@
 #include <stdio.h>
 
 struct node {
-  int x;
-  struct node *next;
+	int x;
+	struct node *next;
 };
 
-void addNode(int x, struct node *head);
-struct node* findLastNode(struct node *someStruct);
-void printlist(struct node *someStruct);
-struct node* findNode(int lookingFor, struct node *headOfNode);
-void removeNode(struct node *toRemove);
 
-    struct node *head;      
 
-int main()
-{
-    /* This will be the unchanging first node */
-    struct node *last;
-    struct node *next;
+struct node *head;      
 
-    // head = addtolist(tmp);
+void initHead(int x){
 
-    /* Now root points to a node struct */
-    head = (struct node*)malloc(sizeof(struct node) ); 
-
-    // /* The node root points to has its next pointer equal to a null pointer 
-    //    set */
-    head->next = NULL;  
- 
-    // /* By using the -> operator, you can modify what the node,
-    //    a pointer, (root in this case) points to. */
-    head->x = 1;    
-   // last = findLastNode(head);
-
-   // next = (struct node*)malloc(sizeof(struct node) );
-    //last->next = next;
-    //next->x = 2;
-    //next->next = NULL;
-    for (int i = 0; i < 5; ++i)
-    {
-    next = (struct node*)malloc(sizeof(struct node) );
-    next->x = i;
-    next->next=NULL;
-    findLastNode(head)->next=next;
-    }
-
-    addNode(333, head);
-    last = findLastNode(head);
-    printf("%d\n", last->x);
-    removeNode(findNode(0, head));
-    
-  printlist(head);
+	head = (struct node*)malloc(sizeof(struct node) ); 
+	head->next = NULL;  
+	head->x = x;   
 }
+
+void isNull(struct node* head){
+	if(head == NULL) printf("its Null\n");
+}
+
 
 
 
@@ -66,32 +34,39 @@ struct node* findLastNode(struct node *someStruct){
 	{
 		return someStruct;
 	}else {
-		 return findLastNode(someStruct->next);
+		return findLastNode(someStruct->next);
 	}
 	//return temp;
 }
 
 
-void printlist(struct node *someStruct){
-	if (someStruct == NULL)
-	{
-		printf("No nodes exist\n");
+void printlist(struct node* head){
+	if (head == NULL)
+	{ 
+		printf("No nodes\n");
+		return;
 	}
-	if (someStruct->next == NULL){
-	printf("%d\n", someStruct->x);
+	if (head->next == NULL){
+		printf("%d\n", head->x);
 	}
-	else { printf("%d\n", someStruct->x);
-	printlist(someStruct->next); }
+	else { printf("%d\n", head->x);
+	printlist(head->next); }
 }
 
 void addNode(int x, struct node *head){
+	if (head == NULL)
+	{
+		initHead(x);
+	}else{
+
 	struct node *next;
 	next = (struct node*)malloc(sizeof(struct node));
 
-	findLastNode(head)->next = next;
 	next->x = x;
 	next->next = NULL;
+	findLastNode(head)->next = next;
 
+}
 }
 
 struct node* findNode(int lookingFor, struct node *headOfNode){
@@ -111,6 +86,8 @@ void removeNode(struct node *toRemove){
 	if(findNode(toRemove->x, head) == NULL) return ;
 	if(toRemove == head) {
 		head = head->next;
+		free(toRemove);
+		return;
 	}
 	struct node *beforeNode;
 	struct node *afterNode;
@@ -125,5 +102,19 @@ void removeNode(struct node *toRemove){
 	beforeNode->next = afterNode;
 
 	free(toRemove);
+
+}
+
+
+
+int main()
+{
+	// printf("%d\n", head->x);
+	for (int i = 0; i < 100; ++i)
+	{
+		addNode(i, head);
+	}
+	removeNode(findNode(99, head));
+	printlist(head);
 
 }
