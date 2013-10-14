3fa69f4a3782b9d60aacca19b23946e0b097faaa
Serge Lysak <serge7lysak@gmail.com>
2013-10-13 18:08:36 -0700

    Adding F.java

--- src/testset/f.java
+++ src/testset/f.java
@@ -1,30 +1,97 @@
-package testset;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Created with IntelliJ IDEA.
- * User: joubin
- * Date: 10/13/13
- * Time: 3:46 PM
- */
-public class f {
-    public f() {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> mainArray = new ArrayList<String>();
-        int numSquares = 4 ;//scanner.nextInt();
-        int numSpaces = 2 ;//scanner.nextInt();
-        int totalWidth = (((numSquares-1)*(numSpaces+1))*2)+1;
-        String mainString = "";
-        for (int i = 0; i <= totalWidth; ++i){
-            mainString += ' ';
-        }
-        System.out.println("|"+mainString+"|");
-        StringBuilder mainStringB = new StringBuilder(mainString);
-        for (int i = 0; i < totalWidth; i=i+2){
-            System.out.println(mainStringB);
-            mainStringB.setCharAt(i, '*');
-        }
-    }
+public class F
+{	
+	private Scanner k = new Scanner(System.in);
+	private int numSquares;
+	private int numSpaces;
+	
+	public F()
+	{
+		start();
+	}
+	
+	private void start()
+	{
+		numSquares = k.nextInt();
+		numSpaces = k.nextInt();
+		
+		int width = 2*(numSquares-1)*(numSpaces+1)+1;
+		
+		for(int j = 1;j<=numSquares-1;j++)
+		{
+			printBefore(j-1);
+			printAsterisks(width-((j-1)*(numSpaces+1)*2));
+			printAfter(j-1);
+			System.out.println("");
+			for(int k = 1;k<=numSpaces;k++)
+			{
+				printBefore(j);
+				printSpaces(width-j*(numSpaces+1)*2);
+				printAfter(j);
+				System.out.println("");
+			}
+		}
+		
+		printBefore(numSquares-1);
+		printAsterisks(1);
+		printAfter(numSquares-1);
+		System.out.println("");
+		
+		for(int j = numSquares-1;j>=1;j--)
+		{
+			for(int k = 1;k<=numSpaces;k++)
+			{
+				printBefore(j);
+				printSpaces(width-j*(numSpaces+1)*2);
+				printAfter(j);
+				System.out.println("");
+			}
+			printBefore(j-1);
+			printAsterisks(width-((j-1)*(numSpaces+1)*2));
+			printAfter(j-1);
+			System.out.println("");
+		}
+		
+	}
+	
+	private void printAsterisks(int x)
+	{
+		for(int i = 1;i<=x;i++)
+		{
+			System.out.print("*");
+		}
+	}
+	
+	private void printSpaces(int x)
+	{
+		for(int i = 1;i<=x;i++)
+		{
+			System.out.print(" ");
+		}
+	}
+	
+	private void printBefore(int x)
+	{
+		for(int j = 1;j<=x;j++)
+		{
+			System.out.print("*");
+			for(int i = 1;i<=numSpaces;i++)
+			{
+				System.out.print(" ");
+			}
+		}
+	}
+	
+	private void printAfter(int x)
+	{
+		for(int j = 1;j<=x;j++)
+		{
+			for(int i = 1;i<=numSpaces;i++)
+			{
+				System.out.print(" ");
+			}
+			System.out.print("*");
+		}
+	}
 }
